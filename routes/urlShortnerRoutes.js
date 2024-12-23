2ab17d4c56c1ea9d1faeba1212ec58de14854b01
jeeva <jeelion22@gmail.com>
2024-12-23 11:37:34 +0530

    add routes for graceful url shorten for users and remove urls

--- routes/urlShortnerRoutes.js
+++ routes/urlShortnerRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const urlShortnerController = require("../Controller/urlShortnerController");
-
+const auth = require("../middleware/auth");
 const urlShortnerRoutes = express.Router();
 
-urlShortnerRoutes.post("/short", urlShortnerController.shortUrl);
-urlShortnerRoutes.get("/:uuid", urlShortnerController.getUrl);
+urlShortnerRoutes.post("/short", auth.hasToken, urlShortnerController.shortUrl);
+urlShortnerRoutes.get("/:uuid", auth.hasToken, urlShortnerController.getUrl);
+urlShortnerRoutes.delete(
+  "/:uuid",
+  auth.isAuth,
+  urlShortnerController.removeUrl
+);
 
 module.exports = urlShortnerRoutes;
