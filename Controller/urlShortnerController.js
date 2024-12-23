f114aa5a3691acf452e3a59f26964dc256b91154
jeeva <jeelion22@gmail.com>
2024-12-23 11:36:26 +0530

    add features graceful for both registered and guest users

--- Controller/urlShortnerController.js
+++ Controller/urlShortnerController.js
@@ -1,15 +1,30 @@
 const URLShortner = require("../models/url");
+const User = require("../models/user");
 
 const urlShortnerController = {
   shortUrl: async (req, res) => {
     try {
+      const userId = req.userId;
       const url = req.body;
 
       const shortUrl = new URLShortner(url);
 
       await shortUrl.save();
 
-      res.status(201).json({ message: "Url shortened successfully" });
+      if (userId) {
+        const user = await User.findByIdAndUpdate(userId);
+
+        user?.urlsShortened.push({
+          urlObjectId: shortUrl._id,
+          shortenedUrlId: shortUrl.shortenedUrl,
+        });
+        await user?.save();
+      }
+
+      res.status(201).json({
+        message: "Url shortened successfully",
+        shortenedUrl: `http://localhost:3000/api/url/${shortUrl.shortenedUrl}`,
+      });
     } catch (error) {
       if (error.status === 400) {
         return res.status(400).json({ message: error.message });
@@ -33,6 +48,44 @@ const urlShortnerController = {
       await shortedUrl.save();
 
       res.redirect(shortedUrl.url);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
+
+  removeUrl: async (req, res) => {
+    try {
+      const userId = req.userId;
+      const uuid = req.params.uuid;
+
+      const user = await User.findById(userId);
+
+      if (!user) {
+        return res.status(400).json({ message: "Invalid credentials" });
+      }
+
+      const docToDelete = user.urlsShortened.find(
+        (item) => item.shortenedUrlId === uuid
+      );
+
+      if (!docToDelete) {
+        return res.status(404).json({ message: "URL not found" });
+      }
+
+      user.urlsShortened = user.urlsShortened.filter(
+        (urlShortened) => urlShortened.shortenedUrlId !== uuid
+      );
+
+      if (docToDelete) {
+        await URLShortner.findOneAndDelete({
+          _id: docToDelete.urlObjectId,
+          shortenedUrl: uuid,
+        });
+      }
+
+      await user.save();
+
+      res.status(204).send();
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
